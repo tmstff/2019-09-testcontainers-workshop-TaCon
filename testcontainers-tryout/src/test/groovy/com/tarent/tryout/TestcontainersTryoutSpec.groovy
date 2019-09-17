a5af8d0eb47c8f5bfcbb7c7f29e70f337f41a1dd
Tim Steffens <tsteffens@tarent.de>
2019-09-17 09:42:57 +0200

    added tryout project for testcontainers

---
+++ testcontainers-tryout/src/test/groovy/com/tarent/tryout/TestcontainersTryoutSpec.groovy
@@ -0,0 +1,13 @@
+package com.tarent.tryout
+
+import spock.lang.Specification
+
+class TestcontainersTryoutSpec extends Specification {
+
+    def "db in container is accessible"() {
+
+        expect:
+        true
+    }
+
+}
